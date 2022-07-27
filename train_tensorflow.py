83e8f6582d583cb70ed9c9c0905b23c6956c6e51
samuelluz.g@gmail.com <samuelluz.g@gmail.com>
2022-07-27 11:53:34 -0300

    Creating featurization step

--- train_tensorflow.py
+++ train_tensorflow.py
@@ -2,75 +2,45 @@
 import numpy as np
 import sys
 import os
+import yaml
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dropout, Flatten, Dense
-from tensorflow.keras import applications
 from tensorflow.keras.callbacks import CSVLogger
 from tqdm.keras import TqdmCallback
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython train_tensorflow.py data-file-path\n")
+    sys.exit(1)
 
 pathname = os.path.dirname(sys.argv[0])
 path = os.path.abspath(pathname)
 
-# dimensions of our images.
-img_width, img_height = 150, 150
-
 top_model_weights_path = 'model.h5'
 train_data_dir = os.path.join('data', 'train')
-validation_data_dir = os.path.join('data', 'validation')
 cats_train_path = os.path.join(path, train_data_dir, 'cats')
 nb_train_samples = 2 * len([name for name in os.listdir(cats_train_path)
                             if os.path.isfile(
                                 os.path.join(cats_train_path, name))])
-nb_validation_samples = 800
-epochs = 10
-batch_size = 10
+
+train_data_path = os.path.join(sys.argv[1], 'bottleneck_features_train.npy')
+validation_data_path = os.path.join(sys.argv[1], 'bottleneck_features_validation.npy')
 
 
-def save_bottlebeck_features():
-    datagen = ImageDataGenerator(
-            rotation_range=40,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            rescale=1./255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True,
-            fill_mode='nearest')
-
-    # build the VGG16 network
-    model = applications.VGG16(include_top=False, weights='imagenet')
-
-    generator = datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_train = model.predict_generator(
-        generator, nb_train_samples // batch_size)
-    np.save(open('bottleneck_features_train.npy', 'wb'),
-            bottleneck_features_train)
-
-    generator = datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'wb'),
-            bottleneck_features_validation)
+params = yaml.safe_load(open("params.yaml"))["featurize"]
+nb_validation_samples = params["nb_validation_samples"]
+batch_size = params["batch_size"]
+params = yaml.safe_load(open("params.yaml"))["train"]
+epochs = params["epochs"]
 
 
 def train_top_model():
-    train_data = np.load(open('bottleneck_features_train.npy', 'rb'))
+    train_data = np.load(open(train_data_path, 'rb'))
     train_labels = np.array(
         [0] * (int(nb_train_samples / 2)) + [1] * (int(nb_train_samples / 2)))
 
-    validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
+    validation_data = np.load(open(validation_data_path, 'rb'))
     validation_labels = np.array(
         [0] * (int(nb_validation_samples / 2)) +
         [1] * (int(nb_validation_samples / 2)))
@@ -92,6 +62,4 @@ def train_top_model():
               callbacks=[TqdmCallback(), CSVLogger("metrics.csv")])
     model.save_weights(top_model_weights_path)
 
-
-save_bottlebeck_features()
 train_top_model()
